05f3018bf9838a48ac205fe2f744f95a30fe3377
Haoxiang Li <haoxiang@Haoxiangs-MacBook-Air.local>
2025-07-23 15:47:48 -0700

    update

---
+++ url.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+for item in $@
+do
+    echo https://raw.githubusercontent.com/pppoe/images-repo/main/$item
+done
--- video2gif.sh
+++ video2gif.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 palette="/tmp/palette.png"
-filters="fps=16,scale=768:-1:flags=lanczos"
+#filters="fps=16,scale=768:-1:flags=lanczos"
+filters="fps=16,scale=960:-1:flags=lanczos"
 
 ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
 ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
